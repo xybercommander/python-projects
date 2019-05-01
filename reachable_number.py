6ce800bc97cf3cf7443d4dc9e0f4c3c871ae6264
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2019-05-01 19:23:22 +0530

    Solved reachable numbers question

---
+++ reachable_number.py
@@ -0,0 +1,15 @@
+import random
+num = int(input())
+num_list = set()
+#adding the first number to the set
+num_list.add(num)
+
+while num > 1:
+    num += 1
+    while(num % 10 == 0):
+        num /= 10
+    num_list.add(num)
+    for i in range(2,10):
+        num_list.add(i)
+
+print(len(num_list))
