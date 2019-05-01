2d12ea0ebb616ac7c7982da59d8ae7233500d867
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2019-05-01 23:38:16 +0530

    learnt a new way to check vowels lel

--- pig_latin.py
+++ pig_latin.py
@@ -2,7 +2,7 @@ word = input('Enter a word: ')
 
 def pig_latin(str):
     first_letter = str[0]
-    if first_letter == 'a' or first_letter == 'e' or first_letter == 'i' or first_letter == 'o' or first_letter == 'u':
+    if first_letter in 'aeiou':
         return str + 'ay'
     else:
         new_word = str[1:]
